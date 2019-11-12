d80020fa23e640c91ca19b50e84d5e9b2cf127c3
nightblade9 <nightbladecodes@gmail.com>
2019-11-11 20:36:07 -0500

    Add physics experiment Add 2D motion, gravity, jumping.

---
+++ physics-experiments/Player.gd
@@ -0,0 +1,30 @@
+extends KinematicBody2D
+
+const GRAVITY = 400
+const MOVE_SPEED = 300
+const JUMP_SPEED = -200
+var velocity = Vector2(0, 0)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	self.velocity.y += delta * GRAVITY
+	self.move_and_slide(velocity)
+
+func _input(event):
+	if event is InputEventKey:
+		if event.scancode == KEY_LEFT or event.scancode == KEY_A:
+			if event.is_pressed():
+				self.velocity.x = -MOVE_SPEED
+			else:
+				self.velocity.x = 0
+		elif event.scancode == KEY_RIGHT or event.scancode == KEY_D:
+			if event.is_pressed():
+				self.velocity.x = MOVE_SPEED
+			else:
+				self.velocity.x = 0
+		if event.scancode == KEY_UP or event.scancode == KEY_W and event.is_pressed():
+			self.velocity.y = JUMP_SPEED
