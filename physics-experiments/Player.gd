aeb3b42e85e0f2b3f4fa89e9167db51719def3f8
nightblade9 <nightbladecodes@gmail.com>
2019-11-12 21:20:11 -0500

    Remove unused duplicate code

--- physics-experiments/Player.gd
+++ physics-experiments/Player.gd
@@ -28,18 +28,3 @@ func _process(delta):
 	self.velocity.y += delta * applied_gravity
 	get_input()
 	velocity = move_and_slide(velocity, Vector2(0, -1))
-
-func _input(event):
-	if event is InputEventKey:
-		if event.scancode == KEY_LEFT or event.scancode == KEY_A:
-			if event.is_pressed():
-				self.velocity.x = -MOVE_SPEED
-			else:
-				self.velocity.x = 0
-		elif event.scancode == KEY_RIGHT or event.scancode == KEY_D:
-			if event.is_pressed():
-				self.velocity.x = MOVE_SPEED
-			else:
-				self.velocity.x = 0
-		if event.scancode == KEY_UP or event.scancode == KEY_W and event.is_pressed():
-			self.velocity.y = JUMP_SPEED
