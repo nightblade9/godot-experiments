cb79d2a04121fbf33280fb14064f20ec3a5bcf5c
nightblade9 <nightbladecodes@gmail.com>
2020-12-08 18:15:54 -0500

    New .NET Core 3.1 application

---
+++ blind-console-test/dotnetcore/BlindConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BlindConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
