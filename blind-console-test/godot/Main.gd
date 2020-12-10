9eed2f79d317482b83b1d9059fe3a965261c646e
nightblade9 <nightbladecodes@gmail.com>
2020-12-09 22:59:24 -0500

    Cards are seeded with correct (random) values

--- blind-console-test/godot/Main.gd
+++ blind-console-test/godot/Main.gd
@@ -5,8 +5,14 @@ func _ready():
 	var shared_data = GameData.new()
 	shared_data.num_open = 0
 	
+	var ids = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6]
+	randomize()
+	ids.shuffle()
+	print(str(ids))
+	
 	for child in get_children():
 		child.initialize(shared_data)
+		child.tile = ids.pop_front()
 
 class GameData extends Resource:
 	var num_open = 0
