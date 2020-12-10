b696bd37c70460c1c0985b0f423d22d71b1cc401
nightblade9 <nightbladecodes@gmail.com>
2020-12-09 23:19:46 -0500

    Matching and stuff works.

--- blind-console-test/godot/Card.gd
+++ blind-console-test/godot/Card.gd
@@ -1,12 +1,16 @@
 extends Area2D
 
 export var tile:int = 1
-var _state:String = "closed" # closed or open
+var _state:String = "closed" # closed, open, or completed
 var _clickable = true
 var _shared_data:Resource
 
 func initialize(shared_data:Resource) -> void:
 	_shared_data = shared_data
+
+func complete():
+	self._state = "complete"
+	self._clickable = false
 
 func _on_Sprite_input_event(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton:
@@ -15,20 +19,22 @@ func _on_Sprite_input_event(_viewport, event, _shape_idx):
 			self._on_toggle()
 
 func _on_toggle() -> void:
-	print("PRE: %s" % _shared_data.num_open)
-	if _shared_data.num_open < 2 and self._state == "closed":
+	if len(_shared_data.open_tiles) < 2 and self._state == "closed":
 		_clickable = false
 		self._state = "open"
 		_update_sprite()
-		_shared_data.num_open += 1
+		_shared_data.open_tiles.append(self)
 		
 func _process(delta):
-	if _shared_data.num_open == 2 and self._state == "open":
-		self._state = "closed"
-		yield(get_tree().create_timer(0.5), "timeout")		
-		_update_sprite()
-		_clickable = true
-		_shared_data.num_open -= 1
+	if self._state != "complete":
+		if len(_shared_data.open_tiles) == 2 and self._state == "open":
+			# if not match, close tiles; open matching is done in Main
+			if _shared_data.open_tiles[0].tile != _shared_data.open_tiles[1].tile: # tiles match
+				self._state = "closed"
+				yield(get_tree().create_timer(0.5), "timeout")
+				_update_sprite()
+				_clickable = true
+				_shared_data.open_tiles.erase(self)
 	
 func _update_sprite():
 	if _state == "closed":
--- blind-console-test/godot/Main.gd
+++ blind-console-test/godot/Main.gd
@@ -1,9 +1,9 @@
 extends Node2D
 
+var _shared_data = GameData.new()
+
 func _ready():
 	ConsoleManager.write_message("Hello, world!")
-	var shared_data = GameData.new()
-	shared_data.num_open = 0
 	
 	var ids = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6]
 	randomize()
@@ -11,8 +11,17 @@ func _ready():
 	print(str(ids))
 	
 	for child in get_children():
-		child.initialize(shared_data)
+		child.initialize(_shared_data)
 		child.tile = ids.pop_front()
+		
+	assert(len(ids) == 0) # gave all IDs away but some are left?!
+
+func _process(delta):
+	if len(_shared_data.open_tiles) == 2 and _shared_data.open_tiles[0].tile == _shared_data.open_tiles[1].tile:
+		for tile in _shared_data.open_tiles:
+			tile.complete()
+		_shared_data.open_tiles.clear()
 
 class GameData extends Resource:
-	var num_open = 0
+	var open_tiles = []
+	var set_complete = false # are we completing a pair and emptying open_tiles?
