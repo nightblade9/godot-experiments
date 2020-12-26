f9631c1ce53e6e570e8f21190a5621ba8ac183d9
nightblade9 <nightbladecodes@gmail.com>
2020-12-26 00:04:39 -0500

    Selection/firing works. Bouncing, doesn't.

--- marble-shooter/Main.gd
+++ marble-shooter/Main.gd
@@ -4,6 +4,9 @@ onready var _arrow:Sprite = $Arrow
 onready var _player_marbles:Node2D = $Player
 onready var _cpu_marbles:Node2D = $CPU
 
+var _selected:Node2D
+var _target:Node2D
+
 func _ready():
 	_arrow.visible = false
 	
@@ -13,9 +16,17 @@ func _ready():
 	for marble in _cpu_marbles.get_children():
 		marble.connect("on_clicked", self, "_on_target_clicked")
 
-func _on_marble_clicked(marble:RigidBody2D) -> void:
+func _on_marble_clicked(marble:KinematicBody2D) -> void:
+	_target = null # clicking who to shoot, deselects target
+	_selected = marble
 	_arrow.visible = true
 	_arrow.position = marble.position - Vector2(0, _arrow.texture.get_height())
 
-func _on_target_clicked(target:RigidBody2D) -> void:
-	print("Target = %s" % target)
+func _on_target_clicked(target:KinematicBody2D) -> void:
+	_target = target
+	if _selected != null:
+		# ready to go
+		_selected.fire_at(_target)
+		_selected = null
+		_target = null
+		_arrow.visible = false
--- marble-shooter/scenes/entities/Marble.gd
+++ marble-shooter/scenes/entities/Marble.gd
@@ -1,7 +1,26 @@
-extends RigidBody2D
+extends KinematicBody2D
 
 signal on_clicked
+signal done_moving
 
+const _IMPULSE:int = 500
+const _STOP_VELOCITY:int = 10
+const _FRICTION:float = 0.99
+
+var _velocity:Vector2 = Vector2.ZERO
+
+func fire_at(target:KinematicBody2D) -> void:
+	var direction = (target.position - self.position).normalized()
+	_velocity = direction * _IMPULSE
+	
+func _physics_process(delta):
+	if _velocity.length() > _STOP_VELOCITY:
+		self.move_and_collide(_velocity * delta)
+		_velocity *= _FRICTION
+	else:
+		_velocity = Vector2.ZERO
+		emit_signal("done_moving", self)
+		
 func _on_SelectionArea_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton:
 		var button = event as InputEventMouseButton
