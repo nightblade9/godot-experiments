40430539907da436ebb4c8372f0f802c8cbbd6b1
nightblade9 <nightbladecodes@gmail.com>
2020-12-26 00:24:14 -0500

    Marble: applying impulse works better than KinematicBody2D

--- marble-shooter/Main.gd
+++ marble-shooter/Main.gd
@@ -16,13 +16,13 @@ func _ready():
 	for marble in _cpu_marbles.get_children():
 		marble.connect("on_clicked", self, "_on_target_clicked")
 
-func _on_marble_clicked(marble:KinematicBody2D) -> void:
+func _on_marble_clicked(marble:Node2D) -> void:
 	_target = null # clicking who to shoot, deselects target
 	_selected = marble
 	_arrow.visible = true
 	_arrow.position = marble.position - Vector2(0, _arrow.texture.get_height())
 
-func _on_target_clicked(target:KinematicBody2D) -> void:
+func _on_target_clicked(target:Node2D) -> void:
 	_target = target
 	if _selected != null:
 		# ready to go
--- marble-shooter/scenes/entities/Marble.gd
+++ marble-shooter/scenes/entities/Marble.gd
@@ -1,33 +1,16 @@
-extends KinematicBody2D
+extends RigidBody2D
 
 signal on_clicked
-signal done_moving
 
-const _IMPULSE:int = 500
+const _IMPULSE:int = 300
 const _STOP_VELOCITY:int = 10
 const _DECAY:float = 0.01
 
-var _velocity:Vector2 = Vector2.ZERO
-
-func fire_at(target:KinematicBody2D) -> void:
+func fire_at(target:Node2D) -> void:
 	var direction = (target.position - self.position).normalized()
-	_velocity = direction * _IMPULSE
+	var impulse = direction * _IMPULSE
+	apply_impulse(Vector2.ZERO, impulse)
 	
-func _physics_process(delta):
-	if _velocity.length() > _STOP_VELOCITY:
-		# bounce code from: https://docs.godotengine.org/en/3.2/tutorials/physics/using_kinematic_body_2d.html#bouncing-reflecting
-		var collision = move_and_collide(_velocity * delta)
-		if collision:
-			_velocity = _velocity.bounce(collision.normal)
-			if collision.collider.has_method("hit"):
-				collision.collider.hit()
-				
-		_velocity *= (1 - _DECAY)
-	else:
-		_velocity = Vector2.ZERO
-		emit_signal("done_moving", self)
-		
-		
 func _on_SelectionArea_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton:
 		var button = event as InputEventMouseButton
