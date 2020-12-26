d3f3af4d9e70b5001b09d3d015c307d865043ed7
nightblade9 <nightbladecodes@gmail.com>
2020-12-26 00:20:12 -0500

    Moving and bouncing works, but doesn't. Bouncing off kinematic bodies doesn't apply force.

--- marble-shooter/scenes/entities/Marble.gd
+++ marble-shooter/scenes/entities/Marble.gd
@@ -5,7 +5,7 @@ signal done_moving
 
 const _IMPULSE:int = 500
 const _STOP_VELOCITY:int = 10
-const _FRICTION:float = 0.99
+const _DECAY:float = 0.01
 
 var _velocity:Vector2 = Vector2.ZERO
 
@@ -15,11 +15,18 @@ func fire_at(target:KinematicBody2D) -> void:
 	
 func _physics_process(delta):
 	if _velocity.length() > _STOP_VELOCITY:
-		self.move_and_collide(_velocity * delta)
-		_velocity *= _FRICTION
+		# bounce code from: https://docs.godotengine.org/en/3.2/tutorials/physics/using_kinematic_body_2d.html#bouncing-reflecting
+		var collision = move_and_collide(_velocity * delta)
+		if collision:
+			_velocity = _velocity.bounce(collision.normal)
+			if collision.collider.has_method("hit"):
+				collision.collider.hit()
+				
+		_velocity *= (1 - _DECAY)
 	else:
 		_velocity = Vector2.ZERO
 		emit_signal("done_moving", self)
+		
 		
 func _on_SelectionArea_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton:
