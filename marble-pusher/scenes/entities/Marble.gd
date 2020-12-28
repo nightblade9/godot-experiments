31744e2528e236117a81eba6bd1895ce102d51d8
nightblade9 <nightbladecodes@gmail.com>
2020-12-27 23:40:00 -0500

    Stop when sufficiently slow

--- marble-pusher/scenes/entities/Marble.gd
+++ marble-pusher/scenes/entities/Marble.gd
@@ -3,12 +3,17 @@ extends RigidBody2D
 signal on_clicked
 
 const _IMPULSE:int = 500
+const _STOP_VELOCITY:int = 50
 const _DECAY:float = 0.01
 
 func fire_at(target:Node2D) -> void:
 	var direction = (target.position - self.position).normalized()
 	var impulse = direction * _IMPULSE
 	apply_impulse(Vector2.ZERO, impulse)
+
+func _physics_process(delta):
+	if self.linear_velocity.length() > 0 and self.linear_velocity.length() <= _STOP_VELOCITY:
+		self.linear_velocity = Vector2.ZERO
 
 func _on_SelectionArea_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton:
