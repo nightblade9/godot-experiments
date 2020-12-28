2f72ce20d7a556b293e87efd6d3958e2dfe0dad3
nightblade9 <nightbladecodes@gmail.com>
2020-12-27 23:34:24 -0500

    Marbles: remove dead filed

--- marble-pusher/scenes/entities/Marble.gd
+++ marble-pusher/scenes/entities/Marble.gd
@@ -3,14 +3,13 @@ extends RigidBody2D
 signal on_clicked
 
 const _IMPULSE:int = 500
-const _STOP_VELOCITY:int = 10
 const _DECAY:float = 0.01
 
 func fire_at(target:Node2D) -> void:
 	var direction = (target.position - self.position).normalized()
 	var impulse = direction * _IMPULSE
 	apply_impulse(Vector2.ZERO, impulse)
-	
+
 func _on_SelectionArea_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton:
 		var button = event as InputEventMouseButton
