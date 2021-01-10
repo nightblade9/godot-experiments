2b4b838e8cce0e12f35b3fa3c2b80d95b3886c51
nightblade9 <nightbladecodes@gmail.com>
2021-01-10 16:25:42 -0500

    Marble: added multiple player balls. It's, interesting

--- marble-physics/scenes/Main.gd
+++ marble-physics/scenes/Main.gd
@@ -1,8 +1,10 @@
 extends Node2D
 
 onready var _fuel_gauge = $UI/FuelBar
-onready var _player = $Game/Player
+onready var _players = $Game/Level/Players
 onready var _turn_label = $UI/TurnLabel
+onready var _goal = $Game/Level/Goal
+
 #onready var _shooters = $Game/Shooters
 
 const _SCORE_MULTIPLIER:int = 500
@@ -11,12 +13,15 @@ var _raw_time_seconds:float = 0
 
 func _ready():
 	if Features.limited_fuel:
-		_fuel_gauge.max_value = _player.MAX_FUEL
+		_fuel_gauge.max_value = _players.get_child(0).MAX_FUEL
 	else:
 		_fuel_gauge.visible = false
 		_turn_label.visible = false
 	
-	_player.setup($UI/SpeedLabel)
+	for player in _players.get_children():
+		player.setup($UI/SpeedLabel)
+	
+	_goal.expect_players(_players.get_child_count())
 
 func _process(delta):
 	_raw_time_seconds += delta
@@ -34,19 +39,25 @@ func _on_Player_used_fuel(fuel_left:float, delta:float):
 func _on_Player_turn_over():
 	# TODO: do stuff, like increment points, apply AI, etc. first
 	# THEN, after AIs etc. are all done, reset to next turn.
-	_player.reset_fuel()
-	_on_Player_used_fuel(_player.MAX_FUEL, 0) # reset gauge
+	for player in _players:
+		player.reset_fuel()
+	_on_Player_used_fuel(_players.get_child(0).MAX_FUEL, 0) # reset gauge
 	
 	_turn_number += 1
 	_turn_label.text = "Turn %s - Fuel: " % _turn_number
 
 
 func _on_Goal_victory():
-	var health_percent = _player.get_health_percent()
+	var total_health = 0.0
+	
+	for player in _players.get_children():
+		total_health += player.get_health_percent()
+	
+	var health_percent = total_health / _players.get_child_count()
+	
 	var final_score = _SCORE_MULTIPLIER * health_percent / _raw_time_seconds
 	$UI/ConclusionLabel.text = ("YOU WIN in %s seconds\nFinal score: %s" % [_raw_time_seconds, final_score])
 	$UI/ConclusionLabel.visible = true
-
 
 func _on_Player_died():
 	$UI/ConclusionLabel.text = ("You DIED!!")
--- marble-physics/scenes/entities/Goal.gd
+++ marble-physics/scenes/entities/Goal.gd
@@ -2,8 +2,14 @@ extends Area2D
 
 signal victory
 
+var _players_left:int = 1
+
+func expect_players(n:int) -> void:
+	_players_left = n
+
 func _on_Goal_body_entered(body:PhysicsBody2D):
 	if body.is_in_group("Player"):
-		self.queue_free()
 		body.queue_free()
-		emit_signal("victory")
+		_players_left -= 1
+		if _players_left <= 0: 
+			emit_signal("victory")
