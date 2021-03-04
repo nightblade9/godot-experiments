1dbdd632a28ea876aa3a04076c54243250d3f3b3
nightblade9 <nightbladecodes@gmail.com>
2021-03-04 10:53:20 -0500

    Change to use locale-specific language codes Still works...

--- Translation/Main.gd
+++ Translation/Main.gd
@@ -1,11 +1,13 @@
 extends Node2D
 
 func _on_Button_pressed():
-	# change to english
+	_change_language("en_US")
 	$Label.text = tr("HELLO")
 
 
 func _on_Button2_pressed():
-	# change to Arabic
+	_change_language("ar_SA")
 	$Label.text = tr("HELLO")
 	
+func _change_language(language):
+	TranslationServer.set_locale(language)
