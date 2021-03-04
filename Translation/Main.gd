02761910d5967708b5915b90c6dd6a3c27025cdf
nightblade9 <nightbladecodes@gmail.com>
2021-03-04 11:28:27 -0500

    Translation with parameters works

--- Translation/Main.gd
+++ Translation/Main.gd
@@ -1,13 +1,17 @@
 extends Node2D
 
+var _times_clicked:int = 0
+
 func _on_Button_pressed():
+	_times_clicked += 1
 	_change_language("en_US")
-	$Label.text = tr("HELLO")
+	$Label.text = tr("HELLO") % _times_clicked
 
 
 func _on_Button2_pressed():
+	_times_clicked += 1	
 	_change_language("ar_SA")
-	$Label.text = tr("HELLO")
+	$Label.text = tr("HELLO") % _times_clicked
 	
 func _change_language(language):
 	TranslationServer.set_locale(language)
