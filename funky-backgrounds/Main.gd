dc4c310087cda69afb54f5ec54f4b32f620deb9a
nightblade9 <nightbladecodes@gmail.com>
2022-02-23 21:50:18 -0500

    Restore "funky backgrounds" (EB-style)
    This reverts commit 84f62f25b9ec0aa29cb42904ac6fd2006ffd4a3a.

---
+++ funky-backgrounds/Main.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+var _total_time:float
+
+func _process(delta):
+	_total_time += delta
+	$vertical.position.y -= delta * 100
+	if $vertical.position.y <= -128:
+		$vertical.position.y += 128
+	$vertical.position.x -= (delta * 20)
+	if $vertical.position.x <= -128:
+		$vertical.position.x += 128
+
+	$horizontal.position.x += delta * 50
+	if $horizontal.position.x >= 0:
+		$horizontal.position.x -= 128
+	$horizontal.position.y += sin(_total_time)
